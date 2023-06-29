cc9fea5d67fd34643c0d1e87e210884aa37b0e9a
shajin47 <105574749+shajin47@users.noreply.github.com>
2023-06-29 09:47:29 +0000

    added create event

--- calendar Automation/Discord/responses.py
+++ calendar Automation/Discord/responses.py
@@ -9,8 +9,28 @@ import findDateIntxt
 
 def handle_response(message) -> str:
     p_message = message.lower()
-    if p_message != '' and p_message != None and p_message[:1] == "/":
-        #type your logic here 
+    if p_message != '' and p_message != None and p_message[0] == '/' :
+
+        if p_message[:3] == "/ce":
+                inp = p_message[4:].split(",")
+
+                e_name = inp[0]
+                st_date = inp[1].replace("-","/").removeprefix(" ")
+                st_date = datetime.strptime(st_date, '%d/%m/%y %H:%M:%S')
+                end_date = (st_date + timedelta(hours=int(inp[2]))).strftime("%Y-%m-%dT%H:%M:%S-07:00")
+                st_date = st_date.strftime("%Y-%m-%dT%H:%M:%S-07:00")
+                # add event function will create event based on given parameter (add more if want)
+                event_id, event_link =  addEvnt.addEvent(st_date,end_date,e_name)
+                return "Event scheduled....... ID: "+ event_id +" Link: "+ event_link
+                # return st_date,end_date,e_name
+        
+        elif "create" in p_message and "event" in p_message:
+
+            # =====store last event 
+            with open('last_event', 'w') as file:
+                file.write('Create Event')
+            # ======get approval
+            return "Sure please provide - EventTitle,Datetime(d/m/y hh:mm:ss)*,duration(in hours)*,Location (use prefix - /CE )"
 
 
 
